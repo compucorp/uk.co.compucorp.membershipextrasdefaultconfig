a940138c7cbe91f92c52a5adb880ecda5554910d
Nishant <nishant.bhorodia@gmail.com>
2019-12-24 18:38:57 +0530

    MAE-173: Added check to see if sample contribution page already exists

--- CRM/Membershipextrasdefaultconfig/Setup/CreateSampleContributionPageStep.php
+++ CRM/Membershipextrasdefaultconfig/Setup/CreateSampleContributionPageStep.php
@@ -6,21 +6,27 @@
 
 class CRM_Membershipextrasdefaultconfig_Setup_CreateSampleContributionPageStep {
 
+  const CONTRIBUTION_PAGE_TITLE = 'Compuclient default contribution page';
+
   /**
    * Main method that will apply all changes.
    * This is called inside public function install of
    * class CRM_Membershipextrasdefaultconfig_Upgrader
    */
   public function apply() {
+    if($this->contributionPageExists()) {
+      return;
+    }
+
     civicrm_api3('ContributionPage', 'create', [
-      'title' => "Compuclient default contribution page",
+      'title' => self::CONTRIBUTION_PAGE_TITLE,
       'financial_type_id' => 1,
       'is_active' => 1,
-      'currency' => "GBP",
+      'currency' => 'GBP',
       'payment_processor' => $this->getActivePaymentProcessors(),
       'is_email_receipt' => 1,
-      'receipt_from_name' => "Compuclient example receipt",
-      'receipt_from_email' => "email@example.com",
+      'receipt_from_name' => 'Compuclient example receipt',
+      'receipt_from_email' => 'email@example.com',
       'is_allow_other_amount' => 1,
     ]);
   }
@@ -33,7 +39,7 @@ class CRM_Membershipextrasdefaultconfig_Setup_CreateSampleContributionPageStep {
 
     $activePaymentProcessors = civicrm_api3('PaymentProcessor', 'get', [
       'sequential' => 1,
-      'return' => ["name"],
+      'return' => ['name'],
       'is_test' => 1,
       'is_active' => 1,
     ]);
@@ -45,6 +51,17 @@ class CRM_Membershipextrasdefaultconfig_Setup_CreateSampleContributionPageStep {
     }
 
     return $paymentProcessorNames;
+  }
+
+  /**
+   * Check if contribution page already exists.
+   */
+  private function contributionPageExists() {
+    $contributionPageCount = civicrm_api3('ContributionPage', 'getcount', [
+      'title' => self::CONTRIBUTION_PAGE_TITLE,
+    ]);
+
+    return $contributionPageCount;
   }
 
 }
