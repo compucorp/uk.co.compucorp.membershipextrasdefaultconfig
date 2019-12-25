c081f0695c595ec2e7583a5a7e28fba629d12cf6
Nishant <nishant.bhorodia@gmail.com>
2019-12-25 18:15:16 +0530

    MAE-173: Add a dummy payment processor when extension is installed

--- CRM/Membershipextrasdefaultconfig/Upgrader.php
+++ CRM/Membershipextrasdefaultconfig/Upgrader.php
@@ -1,6 +1,7 @@
 <?php
 use CRM_Membershipextrasdefaultconfig_ExtensionUtil as E;
 use CRM_Membershipextrasdefaultconfig_Setup_CreateSampleContributionPageStep as CreateSampleContributionPageStep;
+use CRM_Membershipextrasdefaultconfig_Setup_CreateDummyPaymentProcessorStep as CreateDummyPaymentProcessorStep;
 
 /**
  * Collection of upgrade steps.
@@ -9,6 +10,7 @@ class CRM_Membershipextrasdefaultconfig_Upgrader extends CRM_Membershipextrasdef
 
   public function Install() {
     $steps = [
+      new CreateDummyPaymentProcessorStep(),
       new CreateSampleContributionPageStep(),
     ];
 
